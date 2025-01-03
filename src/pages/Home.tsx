b10cb84b9bd643057b31170d6fd8827e2127f282
erwan lorand <erwan.lorand@protonmail.com>
2025-01-03 14:04:18 +0100

    End lesson

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import { useState } from "react";
+
+const getWeatherOfTheDay = () => {
+	return "sunny";
+};
 
 function Home() {
-	return <h1>Hello from Home</h1>;
+	const [weather, setWeather] = useState(null as string | null);
+
+	return (
+		<>
+			<h1>Hello from Home</h1>
+
+			{weather != null ? (
+				<p>Today is a {weather} day</p>
+			) : (
+				<button
+					type="button"
+					onClick={() => {
+						const weatherOfTheDay = getWeatherOfTheDay();
+
+						setWeather(weatherOfTheDay);
+					}}
+				>
+					Get Weather
+				</button>
+			)}
+		</>
+	);
 }
 
 export default Home;
